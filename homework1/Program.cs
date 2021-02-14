cb6ce900a959c74bac8ff3fbc9370d81ed3ba7c1
AliSaad-49 <aliedan94@mail.ru>
2021-02-14 21:08:02 +0300

    HomeWork

---
+++ homework1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+
+            Console.WriteLine("Please, Enter Your Name:");
+            String OwnerName = Console.ReadLine();
+            Console.WriteLine(" Hello  " +OwnerName + " Todays Date is: ");
+           Console.Write( date.ToString("d/M/yyyy"));
+            Console.ReadLine();
+        }
+    }
+}
